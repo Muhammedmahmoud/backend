7e4b755ebda490d698090cb5d37a2d3291692751
Mohammed Mahmoud <loosmm@gmail.com>
2021-05-11 23:47:01 +0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,32 +1,10 @@
 const app = require('express')();
 const { PORT = 4000, SALT = '#ha43-1', LOG_LEVEL, NODE_ENV } = process.env;
-const forecast = require('../utils/forecast')
-const server = http.createServer(app);
-
-const io = require('socket.io')(server, {
-    cors: {
-         origin: "http://localhost:8888",
-         methods: ["GET", "POST"]
-
-    }
-});
-
-io.on('connection', (socket) => {
-    console.log("User Connected")
-    socket.on("Public", (data) => {
-        forecast(data, (error, forecastData) => {
-            if (error) {
-                return console.log(error)
-            }
-            io.emit("weather", forecastData)
-        })
-    });
-})
 
 app.get('/', (req, res) => {
     res.send('Hello');
 });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
 })
