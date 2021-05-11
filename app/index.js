33e44f17293bbe6984c6397f8cb5c44a864693f4
Mohammed Mahmoud <loosmm@gmail.com>
2021-05-11 23:13:58 +0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,44 +1,32 @@
-const crypto = require('crypto');
 const app = require('express')();
 const { PORT = 4000, SALT = '#ha43-1', LOG_LEVEL, NODE_ENV } = process.env;
-const algorithm = 'aes-192-cbc';
-const keylen = 24;
+const forecast = require('./utils/forecast')
+const server = http.createServer(app);
 
-const encrypt = ({ message, secret }) => {
-  const key = crypto.scryptSync(secret, SALT, keylen);
-  const iv = Buffer.alloc(16, 0);
-  const cipher = crypto.createCipheriv(algorithm, key, iv);
+const io = require('socket.io')(server, {
+    cors: {
+         origin: "http://localhost:8888",
+         methods: ["GET", "POST"]
 
-  let encrypted = cipher.update(message, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
+    }
+});
 
-  return encrypted;
-};
-
-const decrypt = ({ message, secret }) => {
-  const key = crypto.scryptSync(secret, SALT, keylen);
-  const iv = Buffer.alloc(16, 0);
-  const decipher = crypto.createDecipheriv(algorithm, key, iv);
-
-  let decrypted = decipher.update(message, 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
-
-  return decrypted;
-};
-
-app.use(({ query: { message, secret }, path }, res, next) => {
-  console.log(`${path}: ${message}`);
-  next();
+io.on('connection', (socket) => {
+    console.log("User Connected")
+    socket.on("Public", (data) => {
+        forecast(data, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            io.emit("weather", forecastData)
+        })
+    });
 })
 
-app.get('/encrypt', ({ query }, res) => {
-  res.send(encrypt(query));
+server.get('/', (req, res) => {
+    res.send('Hello');
 });
 
-app.get('/decrypt', ({ query }, res) => {
-  res.send(decrypt(query));
-});
-
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}, LOG_LEVEL: ${LOG_LEVEL}, NODE_ENV: ${NODE_ENV}`);
-});
+server.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+})
