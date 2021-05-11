f9f1a852c08ed6de4d53517bc9c77de66143e220
Mohammed Mahmoud <loosmm@gmail.com>
2021-05-11 23:52:02 +0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 const app = require('express')();
 const { PORT = 4000, SALT = '#ha43-1', LOG_LEVEL, NODE_ENV } = process.env;
 
-app.get('/', (req, res) => {
+app.get('/hi', (req, res) => {
     res.send('Hello');
 });
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
+    console.log('Example app listening at http://localhost:${PORT}')
 })
